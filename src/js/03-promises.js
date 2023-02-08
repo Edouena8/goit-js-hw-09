8f1ae51df8fe0ebc061d80a40bc5c00a4f25db9a
Edouena8 <103317047+Edouena8@users.noreply.github.com>
2023-02-08 18:51:51 +0100

    created position

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -14,7 +14,7 @@ function onHandleSubmit(evt) {
   const delayStep = +formEl.step.value;
   const amount = +formEl.amount.value;
 
-  for(let position = 0; position < amount; position += 1) {
+  for(let position = 1; position < amount; position += 1) {
 
     createPromise(position, delay)
       .then(({ position, delay }) => {
